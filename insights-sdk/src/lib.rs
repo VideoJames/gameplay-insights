b3afcde2138d12df8e629937135177c1947cdd2f
Jame <lambourn.james@gmail.com>
2026-06-09 17:22:01 +1200

    Added new test for EntitySnapshot

--- insights-sdk/src/lib.rs
+++ insights-sdk/src/lib.rs
@@ -101,4 +101,12 @@ mod tests {
 			.field("damage", FieldValue::Int(10));
 		dbg!(event.summary());
 	}
+	
+	#[test]
+	fn differnt_entity_snapshot_timestamps() {
+		let first = EntitySnapshot::new("Gobble", 0);
+		let second = EntitySnapshot::new("Gobble", 1);
+		dbg!(first.timestamp);
+		dbg!(second.timestamp);
+	}
 }
