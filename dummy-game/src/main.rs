6922d4363e74aa4c2e0ff2e070f513a7173359b5
Jame <lambourn.james@gmail.com>
2026-06-10 19:56:14 +1200

    Added test event building and envelope encoding to dummy-game

--- dummy-game/src/main.rs
+++ dummy-game/src/main.rs
@@ -1,6 +1,24 @@
-use insights_sdk;
-use insights_sdk::{Envelope, Event, FieldValue};
+use std::error::Error;
+use insights_sdk::{ Envelope, Event, FieldValue, Payload};
 
-fn main() {
-    
+fn main() -> Result<(), Box<dyn Error>> {
+    // Build an event
+
+    let event = Event::new("First Event")
+        .field("Reaction Text", FieldValue::Text("React!".to_string()))
+        .field("Firing", FieldValue::Bool(true))
+        .field("Second", FieldValue::Bool(true))
+        .field("Damage", FieldValue::Int(1))
+        .field("X Position", FieldValue::Float(1.5));
+
+    // Wrap it in an Envelope
+    let envelope = Envelope::new(0, Payload::Event(event));
+
+    // Encode
+    let encoded = envelope.encode()?;
+
+    // Print
+    println!("{:?}", encoded);
+
+    Ok(())
 }
