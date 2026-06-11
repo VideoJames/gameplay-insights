e09f4f8723f6639af355f9be1c96b3859adb7926
Jame <lambourn.james@gmail.com>
2026-06-11 12:38:17 +1200

    Connect to TcpStream and send two test events

--- dummy-game/src/main.rs
+++ dummy-game/src/main.rs
@@ -1,24 +1,28 @@
 use std::error::Error;
-use insights_sdk::{ Envelope, Event, FieldValue, Payload};
+use std::net::TcpStream;
+use insights_sdk::{Envelope, Event, FieldValue, Payload};
+use insights_sdk::transport::write_frame;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Build an event
 
-    let event = Event::new("First Event")
+    let event_a = Event::new("First Event")
         .field("Reaction Text", FieldValue::Text("React!".to_string()))
         .field("Firing", FieldValue::Bool(true))
         .field("Second", FieldValue::Bool(true))
         .field("Damage", FieldValue::Int(1))
         .field("X Position", FieldValue::Float(1.5));
 
+    let event_b = Event::new("B Event");
+
     // Wrap it in an Envelope
-    let envelope = Envelope::new(0, Payload::Event(event));
+    let envelope_a = Envelope::new(0, Payload::Event(event_a));
+    let envelope_b = Envelope::new(0, Payload::Event(event_b));
 
-    // Encode
-    let encoded = envelope.encode()?;
+    let mut stream = TcpStream::connect("127.0.0.1:8088")?;
 
-    // Print
-    println!("{:?}", encoded);
+    write_frame(&mut stream, &envelope_a)?;
+    write_frame(&mut stream, &envelope_b)?;
 
     Ok(())
 }
